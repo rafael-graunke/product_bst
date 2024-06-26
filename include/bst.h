415b9a1ba3cf1415c5a1555b7432978fb5f815b2
Lucas Alves <lucas.alves@beemon.com.br>
2024-06-25 21:31:00 -0300

    fix: to ficando maluco

--- include/bst.h
+++ include/bst.h
@@ -16,14 +16,16 @@ typedef struct s_node
 Node *bst_create(Node *parent, char *key, int value);
 bool bst_insert(Node *bst, char *key, int value);
 Node *bst_fetch(Node *bst, char *key);
-void bst_delete(Node *bst, char *key);
+void bst_delete(Node *bst);
 /* Retorna o numero total de produtos (nao as quantidades) */
 int bst_count(Node *bst);
 /* Retorna a soma total de todas as quantidades de produtos */
 int bst_value_count(Node *bst);
+void bst_show_by_letter(Node *bst, char letter);
 void bst_show(Node *bst);
 void bst_show_over(Node *bst, int value);
 void bst_show_below(Node *bst, int value);
 void bst_show_preorder(Node *bst);
+void bst_fetch_print(Node *bst, char *key);
 
 #endif
